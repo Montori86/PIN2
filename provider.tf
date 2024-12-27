93271b4665373964b762a662940353fc705b008e
German <powit10@MacBook-Pro-de-PowIT10.local>
2024-12-27 13:59:56 -0300

    fix terraform init

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_version = ">= 1.6.6"
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31.0"
+      version = ">= 5.31.0"
     }
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"
