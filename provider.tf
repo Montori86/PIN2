31f310439a96dbed3693cfbe57ba26dfb0746a44
German <powit10@MacBook-Pro-de-PowIT10.local>
2024-12-27 13:56:29 -0300

    fix provider

--- provider.tf
+++ provider.tf
@@ -3,12 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.31.0"
+      version = "~> 5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  alias   = "main"
 }
